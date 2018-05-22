a254a37f5a44507bb6648c76987cd24a55acf660
ftsou <38851786+ftsou@users.noreply.github.com>
2018-05-21 20:08:07 -0700

    APM Counter for Higher APM Winners
    Count of Winner APM > Loser APM and vice versa

---
+++ APM_WinOverLossCounter.cpp
@@ -0,0 +1,131 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+//This program calculates if the amount of higher APM winners are greater than higher apm losers
+int main()
+{
+
+	ifstream ifile;
+	ifile.open("fulllog.log");
+	if(!ifile)
+	{
+		cerr<< "Error opening file. \n";
+		return -1;
+	}
+
+	int WinnerAPMOverLoserCount=0;
+	int LoserAPMOverWinnerCount=0;
+	int LoserAndWinnerEquals=0;
+
+	string instring;
+	getline(ifile,instring); //ignore file location
+
+	getline(ifile,instring); //ignore ------ buffer
+
+	getline(ifile,instring); //ignore structure
+
+	size_t pos = 0;
+	int i=0;
+	string filename;
+	int build;
+	string map_name;
+	int game_duration_loops;
+	int players;
+	string p1outcome;
+	string p1race;
+	int p1apm;
+	string p2race;
+	int p2apm;
+
+	while(!ifile.eof())
+	{
+		getline(ifile,instring);
+
+		pos = instring.find(',',0);//filename
+		filename = instring.substr(0, pos);
+		instring = instring.substr(pos + 1);
+
+		pos = instring.find(',',0);//build
+		build = atoi(instring.substr(0,pos).c_str());
+		instring = instring.substr(pos + 1);
+
+		pos = instring.find(',',0);//map_name
+		map_name = instring.substr(0, pos);
+		instring = instring.substr(pos + 1);
+
+		pos = instring.find(',',0);//game_duration_loops
+		game_duration_loops = atoi(instring.substr(0,pos).c_str());
+		instring = instring.substr(pos + 1);		
+
+		pos = instring.find(',',0);//players
+		players = atoi(instring.substr(0,pos).c_str());
+		instring = instring.substr(pos + 1);
+
+		pos = instring.find(',',0);//p1-outcome
+		p1outcome = instring.substr(0, pos);
+		instring = instring.substr(pos + 1);
+
+		pos = instring.find(',',0);//p1-race
+		p1race = instring.substr(0, pos);
+		instring = instring.substr(pos + 1);
+
+		pos = instring.find(',',0);//p1-apm
+		p1apm = atoi(instring.substr(0,pos).c_str());
+		instring = instring.substr(pos + 1);
+
+		pos = instring.find(',',0);//p2-race
+		p2race = instring.substr(0, pos);
+		instring = instring.substr(pos + 1);
+
+		pos = instring.find(',',0);//p2-apm
+		p2apm = atoi(instring.substr(0,pos).c_str());
+		instring = instring.substr(pos + 1);
+
+		if(p1outcome == "Victory")
+		{
+			if(p1apm > p2apm)
+			{
+				WinnerAPMOverLoserCount+=1;
+			}
+			if(p2apm > p1apm)
+			{
+				LoserAPMOverWinnerCount+=1;
+			}
+			if(p2apm==p1apm)
+			{
+				LoserAndWinnerEquals+=1;
+			}			
+
+
+		}
+		else
+		{
+			if(p1apm < p2apm)
+			{
+				WinnerAPMOverLoserCount+=1;
+			}
+			if(p2apm < p1apm)
+			{
+				LoserAPMOverWinnerCount+=1;
+			}
+			if(p2apm==p1apm)
+			{
+				LoserAndWinnerEquals+=1;
+			}			
+	
+		}
+		i++;
+
+	}
+
+
+	cout<< "Winners APM Over Losers APM: " << WinnerAPMOverLoserCount<<endl;
+	cout<< "Losers APM Over Winners Count: " << LoserAPMOverWinnerCount<<endl;
+	cout<< "Winners and Losers APM Equal: " << LoserAndWinnerEquals<<endl;
+	cout<< "Total count: "<< (i-1)<<endl;
+string loop;
+	cin>> loop;
+		return 1;
+
+}
